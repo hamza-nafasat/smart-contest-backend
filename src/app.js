73a996ee2451a53f451af448cee00e8155f76b3e
hamza-nafasat <hamzanafasat4155@gmail.com>
2025-05-19 23:06:00 +0500

    add

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import AuthRoutes from "./routes/auth.routes.js";
+import ContestRoutes from "./routes/contest.routes.js";
+import QuestionRoutes from "./routes/question.routes.js";
+import ParticipationRoutes from "./routes/participation.routes.js";
 import cors from "cors";
 
 const app = express();
@@ -21,6 +24,9 @@ app.use(cookieParser());
 // routes
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/api/auth", AuthRoutes);
+app.use("/api/contests", ContestRoutes);
+app.use("/api/questions", QuestionRoutes);
+app.use("/api/participations", ParticipationRoutes);
 
 // error handler
 app.use(errorHandler);
